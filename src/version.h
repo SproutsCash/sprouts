4cb9d0411a5d6b50abeee28565df988ec511e566
Joey-PC\Joey <5mil@tuta.io>
2017-11-12 21:49:38 -0500

    Versioning?

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #define PEERUNITY_VERSION_MAJOR       0
 #define PEERUNITY_VERSION_MINOR       1
 #define PEERUNITY_VERSION_REVISION    2
-#define PEERUNITY_VERSION_BUILD       2
+#define PEERUNITY_VERSION_BUILD       3
 
 static const int PEERUNITY_VERSION =
                            1000000 * PEERUNITY_VERSION_MAJOR
@@ -29,7 +29,7 @@ static const int PEERUNITY_VERSION =
 #define PPCOIN_VERSION_MAJOR       0
 #define PPCOIN_VERSION_MINOR       4
 #define PPCOIN_VERSION_REVISION    2
-#define PPCOIN_VERSION_BUILD       2
+#define PPCOIN_VERSION_BUILD       0
 
 static const int PPCOIN_VERSION =
                            1000000 * PPCOIN_VERSION_MAJOR
@@ -60,7 +60,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60004;
+static const int PROTOCOL_VERSION = 70000;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 // NOTE: as of bitcoin v0.6 message serialization (vSend, vRecv) still
@@ -69,11 +69,11 @@ static const int MIN_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 69999;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
+static const int NOBLKS_VERSION_END = 69999;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
